4e9f42508b98fa0a320ecb99c29a03572ca4cae5
NikolaPrusacManahy <Nikolamanahy12@gmail.com>
2025-11-14 19:31:01 +0000

    refactor: collision detection for ball function

--- 16_SFML_Games/arkanoid.cpp
+++ 16_SFML_Games/arkanoid.cpp
@@ -7,63 +7,26 @@ const int SCREEN_WIDTH = 520;
 const int SCREEN_HEIGHT = 450;
 const int TARGET_FPS = 60;
 
-int arkanoid()
+void cheackBallCollision(float& ballPosX, float& ballPosY,
+    float& ballVelocityX, float& ballVelocityY,
+    Sprite* blocks, int& blockCount)
 {
-    srand(time(0));
+    ballPosX += ballVelocityX;
+    for (int i = 0; i < blockCount; i++)
+        if (FloatRect(ballPosX + 3, ballPosY + 3, 6, 6).intersects(blocks[i].getGlobalBounds()))
+        {
+            blocks[i].setPosition(-100, 0); ballVelocityX = -ballVelocityX;
+        }
 
-    RenderWindow window(VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Arkanoid!");
-    window.setFramerateLimit(TARGET_FPS);
+    ballPosY += ballVelocityY;
+    for (int i = 0; i < blockCount; i++)
+        if (FloatRect(ballPosX + 3, ballPosY + 3, 6, 6).intersects(blocks[i].getGlobalBounds()))
+        {
+            blocks[i].setPosition(-100, 0); ballVelocityY = -ballVelocityY;
+        }
 
-    Texture block_txr,background_txr,ball_txr,paddle_txr;
-    block_txr.loadFromFile("images/arkanoid/block01.png");
-    background_txr.loadFromFile("images/arkanoid/background.jpg");
-    ball_txr.loadFromFile("images/arkanoid/ball.png");
-    paddle_txr.loadFromFile("images/arkanoid/paddle.png");
-
-    Sprite backgroundSprite(background_txr), ballSprite(ball_txr), paddleSprite(paddle_txr);
-    paddleSprite.setPosition(300,440);
-
-    const int MAX_BLOCKS = 1000;
-    Sprite blocks[MAX_BLOCKS];
-
-    int blockCount = 0;
-    setupBlock(blocks, block_txr,blockCount);
-    
-    while (window.isOpen())
-    {
-       Event event;
-       while (window.pollEvent(event))
-       {
-         if (event.type == Event::Closed)
-             window.close();
-       }
-        float ballVelocityX=6, ballVelocityY=5;
-        float xPos=300, yPos=300;
-
-        xPos+=ballVelocityX;
-        for (int i=0;i<blockCount;i++)
-            if ( FloatRect(xPos+3,yPos+3,6,6).intersects(blocks[i].getGlobalBounds()) ) 
-                {blocks[i].setPosition(-100,0); ballVelocityX=-ballVelocityX;}
-
-        yPos+=ballVelocityY;
-        for (int i=0;i<blockCount;i++)
-            if ( FloatRect(xPos+3,yPos+3,6,6).intersects(blocks[i].getGlobalBounds()) ) 
-                {blocks[i].setPosition(-100,0); ballVelocityY=-ballVelocityY;}
-
-        if (xPos<0 || xPos>520)  ballVelocityX=-ballVelocityX;
-        if (yPos<0 || yPos>450)  ballVelocityY=-ballVelocityY;
-
-        if (Keyboard::isKeyPressed(Keyboard::Right)) paddleSprite.move(6,0);
-        if (Keyboard::isKeyPressed(Keyboard::Left)) paddleSprite.move(-6,0);
-
-        if ( FloatRect(xPos,yPos,12,12).intersects(paddleSprite.getGlobalBounds()) ) ballVelocityY=-(rand()%5+2);
-
-        ballSprite.setPosition(xPos,yPos);
-
-        drawWindow(window, backgroundSprite, ballSprite, paddleSprite, blocks, blockCount);
-    }
-
-  return 0;
+    if (ballPosX < 0 || ballPosX>520)  ballVelocityX = -ballVelocityX;
+    if (ballPosY < 0 || ballPosY>450)  ballVelocityY = -ballVelocityY;
 }
 
 void drawWindow(RenderWindow& window, Sprite& backgroundSprite, Sprite& ballSprite, Sprite& paddleSprite, Sprite* t_blocks, int& blockCount)
@@ -97,3 +60,53 @@ void setupBlock(Sprite* t_blocks, Texture& t_block_txr, int& t_blockCount)
         }
     }
 }
+
+int arkanoid()
+{
+    srand(time(0));
+
+    RenderWindow window(VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Arkanoid!");
+    window.setFramerateLimit(TARGET_FPS);
+
+    Texture block_txr,background_txr,ball_txr,paddle_txr;
+    block_txr.loadFromFile("images/arkanoid/block01.png");
+    background_txr.loadFromFile("images/arkanoid/background.jpg");
+    ball_txr.loadFromFile("images/arkanoid/ball.png");
+    paddle_txr.loadFromFile("images/arkanoid/paddle.png");
+
+    Sprite backgroundSprite(background_txr), ballSprite(ball_txr), paddleSprite(paddle_txr);
+    paddleSprite.setPosition(300,440);
+
+    const int MAX_BLOCKS = 1000;
+    Sprite blocks[MAX_BLOCKS];
+
+    int blockCount = 0;
+    setupBlock(blocks, block_txr,blockCount);
+
+    float ballVelocityX = 6, ballVelocityY = 5;
+    float ballPosX = 300, ballPosY = 300;
+    
+    while (window.isOpen())
+    {
+       Event event;
+       while (window.pollEvent(event))
+       {
+         if (event.type == Event::Closed)
+             window.close();
+       }
+       cheackBallCollision(ballPosX, ballPosY, ballVelocityX, ballVelocityY, blocks, blockCount);
+
+        if (Keyboard::isKeyPressed(Keyboard::Right)) paddleSprite.move(6,0);
+        if (Keyboard::isKeyPressed(Keyboard::Left)) paddleSprite.move(-6,0);
+
+        if ( FloatRect(ballPosX,ballPosY,12,12).intersects(paddleSprite.getGlobalBounds()) ) ballVelocityY=-(rand()%5+2);
+
+        ballSprite.setPosition(ballPosX,ballPosY);
+
+        drawWindow(window, backgroundSprite, ballSprite, paddleSprite, blocks, blockCount);
+    }
+
+  return 0;
+}
+
+
